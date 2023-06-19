e73e2eaaa9893640fc7877715c53f3eb2506fb7a
Jenaide Sibolie <jenaidesibolie@gmail.com>
2023-06-19 20:58:07 +0200

    added github workflow

--- pw_base/settings.py
+++ pw_base/settings.py
@@ -11,10 +11,15 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+try:
+    SOME_SECRET = os.environ["SOME_SECRET"]
+except KeyError:
+    SOME_SECRET = "SECRET NOT AVAILABLE"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
